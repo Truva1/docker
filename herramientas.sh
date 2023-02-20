c2d928648993ee723754c2cdef6d935f615efd0b
Truva1 <67235905+Truva1@users.noreply.github.com>
2023-02-20 15:44:19 -0500

    Update herramientas.sh

--- herramientas.sh
+++ herramientas.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 echo "DOCKER"
+sudo apt-get update
 sudo apt update
-sudo apt update
-sudo apt install docker.io
+sudo apt-get install docker.io
 echo "PIP"
 sudo apt install python3 -y
 sudo apt install python3-pip -y
